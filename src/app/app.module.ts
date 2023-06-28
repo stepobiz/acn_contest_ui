c5d1fbf664efc2cd1f5a3f5d298e8b83d494bbb2
Cavallaro Catello Stefano <stefano@stepo.biz>
2023-06-28 14:10:06 +0200

    Initial config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,8 +10,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+
+    AppRoutingModule,
+	
   ],
   providers: [],
   bootstrap: [AppComponent]
